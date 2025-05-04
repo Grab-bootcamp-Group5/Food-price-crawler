9bba732e495c5bfb9c54cf339701133215a6147e
cuderbk <richardbui26k4@gmail.com>
2025-05-04 09:41:12 +0700

    feat: bhx fetch category and url

--- crawler/stores/run_products.py
+++ crawler/stores/run_products.py
@@ -21,18 +21,9 @@ async def async_main(domain: str):
         c for c in module.__dict__.values()
         if isinstance(c, type) and hasattr(c, "chain") and c.chain == domain
     )
-    crawler = crawler_cls(store_id=571)
-    products = await crawler.crawl_prices()
-    # if not products:
-    #     print("No prices found.")
-    #     return
+    crawler = crawler_cls()
+    await crawler.crawl_prices()
 
-    # inserted_count = 0
-    # for product in products:
-    #     await upsert_product(product)
-    #     inserted_count += 1
-
-    print(f"Inserted/updated {inserted_count} products into MongoDB")
 
 
 if __name__ == "__main__":
